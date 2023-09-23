e62b89ca80aa5ec15c88e2c3dce6d5593c811146
twincacca <twincacca@users.noreply.github.com>
2023-09-23 16:52:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 rm out*
 dd=$(date | sed 's/ /-/g')
-echo Ciaociao > out-$dd.txt
-date >> out-$dd.txt
-cat user_input.txt | cowsay >> out-$dd.txt
+#echo Ciaociao > out-$dd.txt
+#date >> out-$dd.txt
+#cat user_input.txt | cowsay >> out-$dd.txt
+
+wget http://spalv.ch/it/animali
+wait
+egrep -i "muralto|minusio|locarno" animali --color > out-$dd.txt
