f862628586369b975fff0456fbd8cd74cbd4299f
twincacca <twincacca@users.noreply.github.com>
2023-10-23 12:47:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 
-#rm old-out* diff-*
-rm diff-*
+rm old-out* diff-*
 
 # dd=$(date | sed 's/ /-/g')
 dd=$(env TZ=Europe/Berlin date | sed 's/ /-/g')
@@ -16,7 +15,7 @@ wget http://spalv.ch/it/animaliwait
 egrep -i "muralto|minusio|tenero|locarno" animali* --color | sed 's/ /\n/g' | grep href | grep -v "><img" > out-cani-gatti-$dd.txt
 rm animali*
 diff old-out-cani-gatti-* out-cani-gatti-* > diff-cani-gatti-$dd.txt
-rm -r out-cani-gatti-*
+#rm -r out-cani-gatti-*
 
 # check pigiama aldi
 rename 's/out-pigiama-aldi-/old-out-pigiama-aldi-/'
@@ -25,7 +24,7 @@ wait
 grep -i "pigiama" -r www.aldi-suisse.ch  > out-pigiama-aldi-$dd.txt
 rm -r www.aldi-suisse.ch
 diff old-out-pigiama-aldi-* out-pigiama-aldi-* > diff-pigiama-aldi-$dd.txt
-rm -r out-pigiama-aldi-*
+#rm -r out-pigiama-aldi-*
 
 # check pigiama lidl
 rename 's/out-pigiama-lidl-/old-out-pigiama-lidl-/'
@@ -34,4 +33,4 @@ wait
 find www.lidl.ch | grep Current_Sales_Week | egrep -i "pigiama|pijama|pyjama"> out-pigiama-lidl-$dd.txt
 rm -r www.lidl.ch
 diff old-out-pigiama-lidl-* out-pigiama-lidl-* > diff-pigiama-lidl-$dd.txt
-rm -r out-pigiama-lidl-*
+#rm -r out-pigiama-lidl-*
