e7c9e8bce266cd81a17e39879fbc3d1f7ee3958c
twincacca <twincacca@users.noreply.github.com>
2023-10-29 21:42:59 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,19 +18,19 @@ diff old-out-cani-gatti-* out-cani-gatti-* > diff-cani-gatti-$dd.txt
 #rm -r out-cani-gatti-*
 
 # check pigiama aldi
-#rename 's/out-pigiama-aldi-/old-out-pigiama-aldi-/'
-#wget -r -l1  https://www.aldi-suisse.ch/it/promozioni/promozioni-e-offerte-attuali/
-#wait
-#grep -i "pigiama" -r www.aldi-suisse.ch  > out-pigiama-aldi-$dd.txt
-#rm -r www.aldi-suisse.ch
-#diff old-out-pigiama-aldi-* out-pigiama-aldi-* > diff-pigiama-aldi-$dd.txt
-##rm -r out-pigiama-aldi-*
+rename 's/out-pigiama-aldi-/old-out-pigiama-aldi-/'
+wget -r -l1  https://www.aldi-suisse.ch/it/promozioni/promozioni-e-offerte-attuali/
+wait
+grep -i "pigiama" -r www.aldi-suisse.ch  > out-pigiama-aldi-$dd.txt
+rm -r www.aldi-suisse.ch
+diff old-out-pigiama-aldi-* out-pigiama-aldi-* > diff-pigiama-aldi-$dd.txt
+#rm -r out-pigiama-aldi-*
 
 # check pigiama lidl
-rename 's/out-pigiama-lidl-/old-out-pigiama-lidl-/'
-wget -r -l2  https://www.lidl.ch/
-wait
-find www.lidl.ch | grep Current_Sales_Week | egrep -i "pigiama|pijama|pyjama"> out-pigiama-lidl-$dd.txt
-rm -r www.lidl.ch
-diff old-out-pigiama-lidl-* out-pigiama-lidl-* > diff-pigiama-lidl-$dd.txt
-#rm -r out-pigiama-lidl-*
+#rename 's/out-pigiama-lidl-/old-out-pigiama-lidl-/'
+#wget -r -l2  https://www.lidl.ch/
+#wait
+#find www.lidl.ch | grep Current_Sales_Week | egrep -i "pigiama|pijama|pyjama"> out-pigiama-lidl-$dd.txt
+#rm -r www.lidl.ch
+#diff old-out-pigiama-lidl-* out-pigiama-lidl-* > diff-pigiama-lidl-$dd.txt
+##rm -r out-pigiama-lidl-*
