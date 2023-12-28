86ccac2db283bf0da67a8dc92549678daccacda7
twincacca <twincacca@users.noreply.github.com>
2023-12-28 16:38:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,12 +8,5 @@ dd=$(env TZ=Europe/Berlin date | sed 's/ /-/g')
 #date >> out-$dd.txt
 #cat user_input.txt | cowsay >> out-$dd.txt
 
-# check cani/gatti smarriti in zona
-rename 's/out-cani-gatti-/old-out-cani-gatti-/' *
-#wget http://spalv.ch/it/animaliwait
 wget https://www.spalv.ch/it/animali/smarriti
-egrep -i "muralto|minusio|tenero|locarno" smarriti --color | sed 's/ /\n/g' | grep href | grep -v "><img" > out-cani-gatti-$dd.txt
-rm smarriti
-diff old-out-cani-gatti-* out-cani-gatti-* > diff-cani-gatti-$dd.txt
-#rm -r out-cani-gatti-*
 
