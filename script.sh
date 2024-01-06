8a01b46d92e53e02e450f7d07a52ecdca3020555
twincacca <twincacca@users.noreply.github.com>
2024-01-06 15:45:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,13 +11,13 @@ dd=$(env TZ=Europe/Berlin date | sed 's/ /-/g')
 # check cani/gatti smarriti in zona
 #rename 's/out-cani-gatti-/old-out-cani-gatti-/' * # non va piu' rename, allora farne uno io:
 #bash rename.sh
-#-------- rm -r out-cani-gatti-* ---------
+#-------- rename out-cani-gatti-* ---------
 ls out-cani-gatti-* | awk '{print "mv " $1 " old-"$1}' > run.sh
 bash run.sh
 rm run.sh
-#-------- rm -r out-cani-gatti-* ---------END
+#-------- rename out-cani-gatti-* ---------END
 
-wget http://spalv.ch/it/animaliwait
+wget https://www.spalv.ch/it/animali
 egrep -i "muralto|minusio|tenero|locarno" animali* --color | sed 's/ /\n/g' | grep href | grep -v "><img" > out-cani-gatti-$dd.txt
 rm animali*
 diff old-out-cani-gatti-* out-cani-gatti-* > diff-cani-gatti-$dd.txt
