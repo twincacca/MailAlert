c12456e61d0fbb5966acbbe70c2f8aaa247f2d11
twincacca <twincacca@users.noreply.github.com>
2024-01-22 16:41:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,5 @@
-rm old-out* diff-*
-
-# dd=$(date | sed 's/ /-/g')
 dd=$(env TZ=Europe/Berlin date | sed 's/ /-/g')
-#dd=$(env TZ=Europe/Berlin date +%Y%m%d  | sed 's/ /-/g')
-
-#echo Ciaociao > out-$dd.txt
-#date >> out-$dd.txt
-#cat user_input.txt | cowsay >> out-$dd.txt
-
-# check cani/gatti smarriti in zona
-#rename 's/out-cani-gatti-/old-out-cani-gatti-/' * # non va piu' rename, allora farne uno io:
-#bash rename.sh
-#-------- rename out-cani-gatti-* ---------
-ls out-cani-gatti-* | awk '{print "mv " $1 " old-"$1}' > run.sh
-bash run.sh
-rm run.sh
-#-------- rename out-cani-gatti-* ---------END
 
 wget https://www.spalv.ch/it/animali/smarriti
 egrep -i "muralto|minusio|tenero|locarno" smarriti* --color | sed 's/ /\n/g' | grep href | grep -v "><img" > out-cani-gatti-$dd.txt
-rm smarriti*
-diff old-out-cani-gatti-* out-cani-gatti-* > diff-cani-gatti-$dd.txt
-#rm -r out-cani-gatti-*
 
