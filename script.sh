0dfcffff5965e4472e367f4f7f347e47685ab66e
twincacca <twincacca@users.noreply.github.com>
2024-01-22 17:26:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 dd=$(env TZ=Europe/Berlin date | sed 's/ /-/g')
 rm out-*
 rm smarriti*
+
+#-------- rename out-cani-gatti-* ---------
+ls out-cani-gatti-* | awk '{print "mv " $1 " old-"$1}' > run.sh
+bash run.sh
+rm run.sh
+#-------- rename out-cani-gatti-* ---------END
+
 wget https://www.spalv.ch/it/animali/smarriti
 egrep -i "muralto|minusio|tenero|locarno" smarriti* --color | sed 's/ /\n/g' | grep href | grep -v "><img" > out-cani-gatti-$dd.txt
 
